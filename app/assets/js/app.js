dc4006b189a8e451c7abd0c18a58ae1725ab9180
Karafizi Arthur <karafizi.artur@gmail.com>
2016-03-27 22:57:17 +0300

    Adding toggle for Login and Map popaps

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -50,7 +50,7 @@
     }
 })();
 // Add load listener
-window.addEventListener('load', function load(event) {
+window.addEventListener('load', function load() {
     // Remove load listener
     window.removeEventListener('load', load, false);
     setTimeout(function () {
@@ -63,6 +63,15 @@ window.addEventListener('load', function load(event) {
         // Remove "overflow: hidden" style from body
         $('body').removeAttr('style');
     }, 1500);
+    $('.modal-overlay, .modal-content-close').click(function () {
+        $('.modal-content-login, .modal-content-map, .modal-overlay').fadeOut(300);
+    });
+    $('button.login').click(function () {
+        $('.modal-overlay, .modal-content-login').fadeIn(300);
+    });
+    $('button.open-map').click(function () {
+        $('.modal-overlay, .modal-content-map').fadeIn(300);
+    });
 });
 angular
     .module('barbershop')
--- app/dev/custom.ts
+++ app/dev/custom.ts
@@ -1,5 +1,5 @@
 // Add load listener
-window.addEventListener('load', function load(event) {
+window.addEventListener('load', function load() {
   // Remove load listener
   window.removeEventListener('load', load, false);
 
@@ -16,5 +16,17 @@ window.addEventListener('load', function load(event) {
     $('body').removeAttr('style');
 
   }, 1500);
+
+  $('.modal-overlay, .modal-content-close').click(function() {
+    $('.modal-content-login, .modal-content-map, .modal-overlay').fadeOut(300);
+  });
   
+  $('button.login').click(function() {
+    $('.modal-overlay, .modal-content-login').fadeIn(300);
+  });
+
+  $('button.open-map').click(function() {
+    $('.modal-overlay, .modal-content-map').fadeIn(300);
+  });
+
 });
